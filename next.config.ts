3f277eda4d776e4bae71f046ad8e82652f9ce2a7
iamseung <seesungseok@naver.com>
2025-06-21 14:41:13 +0900

    rewrite 변경

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://43.200.94.63/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
